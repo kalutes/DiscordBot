e5301827f2a22f2378a0f8c94879776b112f0309
Kameron Lutes <kameo315@gmail.com>
2021-02-12 15:37:10 -0800

    Requirements

--- bot.py
+++ bot.py
@@ -14,27 +14,26 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='8=D')
 
 @bot.event
 async def on_voice_state_update(member, before, after):
     channel = after.channel
 
-    if member.name == "honey boy":
-        if channel is not None:
-            voice_client = await channel.connect()
-            audio_source = discord.FFmpegPCMAudio('bigmama.mp3')
+    if channel is not None:
+        voice_client = await channel.connect()
+        audio_source = discord.FFmpegPCMAudio(member.name + '.mp3')
 
-            def disconnect(error):
-                coro = voice_client.disconnect()
-                fut = asyncio.run_coroutine_threadsafe(coro, bot.loop)
-                try:
-                    fut.result()
-                except:
-                    # an error happened sending the message
-                    pass
+        def disconnect(error):
+            coro = voice_client.disconnect()
+            fut = asyncio.run_coroutine_threadsafe(coro, bot.loop)
+            try:
+                fut.result()
+            except:
+                # an error happened sending the message
+                pass
 
-            if not voice_client.is_playing():
-                voice_client.play(audio_source, after=disconnect)
+        if not voice_client.is_playing():
+            voice_client.play(audio_source, after=disconnect)
 
 bot.run(TOKEN)
