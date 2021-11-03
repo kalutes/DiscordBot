39cd21e0bc7c339f30a6ac0821adee2d87d709b8
Kameron Lutes <kameo315@gmail.com>
2021-11-03 07:54:48 +0000

    Many changes to moon

--- moon.py
+++ moon.py
@@ -7,6 +7,8 @@ import discord
 from discord.ext import commands
 from discord.ext import tasks
 from gtts import gTTS
+from bs4 import BeautifulSoup
+import asyncio
 
 load_dotenv()
 TOKEN = os.getenv('MOON_TOKEN')
@@ -15,42 +17,180 @@ PREFIX = os.getenv('MOON_PREFIX')
 print("Initializing bot with prefix")
 print(PREFIX)
 
-def shib_price():
-    page = requests.get("https://finance.yahoo.com/quote/SHIB-USD/")
-    x = page.text.find("Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)")
-    num = page.text[x+61:x+69]
-    return(num)
+class Security:
+    symbol = ""
+    valid = False
+    market_hours_price = ""
+    market_hours_change = ""
+    ah_price = ""
+    ah_change = ""
+    volume = ""
 
-async def say_shib(the_bot, channel):
-    price = shib_price()
-    tts = gTTS(text='the price of shiba is {}. To the moon!'.format(price), lang='en', slow=False)
-    tts.save("/media/audio/shib.mp3")
+    def get_yahoo_page(self):
+        return requests.get("https://finance.yahoo.com/quote/{}/".format(self.symbol), headers={'Cache-Control': 'no-cache', "Pragma": "no-cache"})
 
-    for vc in the_bot.voice_clients:
-        await vc.disconnect()
+    def is_valid(self, page):
+        soup = BeautifulSoup(page.text, 'html.parser')
+        div = soup.find("div", {"id": "quote-market-notice"})
 
-    voiceClient = await channel.connect()
+        if div is not None:
+            return True
+
+        return False
+
+    def has_ah_data(self, page):
+        soup = BeautifulSoup(page.text, 'html.parser')
+        div = soup.find("div", {"id": "quote-market-notice"})
+        
+        return len(div.parent.parent.findChildren("div" , recursive=False)) > 1
+
+    def populate_data(self, page):
+        soup = BeautifulSoup(page.text, 'html.parser')
+
+        self.volume = soup.find("td", {"data-test": "TD_VOLUME-value"}).findChildren("span" , recursive=False)[0].text
+        self.market_hours_price = soup.find("div", {"id": "quote-market-notice"}).parent.findChildren("span" , recursive=False)[0].text
+        self.market_hours_change = soup.find("div", {"id": "quote-market-notice"}).parent.findChildren("span" , recursive=False)[1].text
+
+        if self.has_ah_data(page):
+            self.ah_price = soup.find("div", {"id": "quote-market-notice"}).parent.parent.findChildren("div" , recursive=False)[1].findChildren("span" , recursive=False)[0].text
+            self.ah_change = soup.find("div", {"id": "quote-market-notice"}).parent.parent.findChildren("div" , recursive=False)[1].findChildren("span" , recursive=False)[1].text
+        
+        return
+    
+    def __init__(self, symbol):
+        self.symbol = symbol
+
+        page = self.get_yahoo_page()
+
+        if self.is_valid(page):
+            self.valid = True
+            self.populate_data(page)
+
+async def say_price(the_bot, channel, vc, symbol):
+    audio_file = "audio.mp3"
+
+    speak_text = "Invalid ticker or security symbol"
+
+    sec_data = Security(symbol)
+
+    if sec_data.valid:
+        price_to_read = sec_data.market_hours_price
+        change_to_read = sec_data.market_hours_change
+        ah_disclaimer = ""
+
+        if sec_data.ah_price != "":
+            price_to_read = sec_data.ah_price
+            change_to_read = sec_data.ah_change
+            ah_disclaimer = "After Hours: "
+
+        speak_text = '{}:{} {}.'.format(sec_data.symbol, ah_disclaimer, price_to_read)
+
+    print(speak_text)
+
+    tts = gTTS(text=speak_text, lang='en', slow=False)
+    tts.save(audio_file)
+
+    if vc is None:
+        vc = await channel.connect()
+
+    if vc.is_playing():
+        return
 
     # Audio choices are stored as a list where the first index is the actual file name in the audio path
-    audioSource = discord.FFmpegPCMAudio('/media/audio/shib.mp3')
+    audioSource = discord.FFmpegPCMAudio(audio_file)
 
-    voiceClient.play(audioSource)
+    vc.play(audioSource)
 
-class ShibSayer(commands.Cog):
+    while vc.is_playing():
+        await asyncio.sleep(1)
+
+def is_valid_symbol(symbol):
+    sec_data = Security(symbol)
+    return sec_data.valid
+
+class PriceSayer(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.try_say_shib.start()
+        self.say_price_interval.start()
+        self.symbols = []
+        self.channel = None
 
-    @commands.command(name='shib', help='Prints the current SHIB price')
-    async def shib_price(self, ctx):
-        channel = ctx.author.voice.channel
-        if channel is not None:
-            await say_shib(self.bot, channel)
+    async def say_all_prices(self):
+        if self.channel is None:
+            return
+
+        vc = None
+
+        for symbol in self.symbols:
+            if len(self.bot.voice_clients) > 0:
+                vc = self.bot.voice_clients[0]
+            await say_price(self.bot, self.channel, vc, symbol)
+
+    @commands.command(name='watch', help='Watches the price of the security')
+    async def price(self, ctx, symbol):
+        if not is_valid_symbol(symbol):
+            await ctx.send("Invalid ticker or security symbol")
+            return
+
+        self.symbols.append(symbol)
+        self.channel = ctx.author.voice.channel
+        await ctx.send("{} will be watched".format(symbol))
+
+    @commands.command(name='say', help='Says the prices of the watched symbols')
+    async def say_prices(self, ctx):
+        await self.say_all_prices()
 
     @commands.command(name='leave', help='Disconnects')
     async def leave_voice(self, ctx):
         for vc in self.bot.voice_clients:
             await vc.disconnect()
+
+        self.channel = None
+
+    @commands.command(name='join', help='Sets the current channel as the channel to join')
+    async def join_channel(self, ctx):
+        self.channel = ctx.author.voice.channel
+        await self.say_all_prices()
+
+    @commands.command(name='clear', help='Clears the list of symbols to watch')
+    async def clear_symbols(self, ctx):
+        self.symbols = []
+
+    @commands.command(name='list', help='Lists the watched symbols')
+    async def list_symbols(self, ctx):
+        if len(self.symbols) == 0:
+            await ctx.send("No symbols are being watched.")
+            return
+
+        message = "Currently watching:\r\n"
+
+        for symbol in self.symbols:
+            message += symbol
+            message += "\r\n"
+
+        await ctx.send(message)
+
+    @commands.command(name='remove', help='Removes the specified symbol from the list of watched symbols')
+    async def remove_symbols(self, ctx, symbol):
+        i = len(self.symbols)
+        self.symbols.remove(symbol)
+
+        if len(self.symbols) == i:
+            await ctx.send("{} was not in the list of watched symbols.".format(symbol))
+            return
+
+        await ctx.send("{} was removed from the list".format(symbol))
+
+    @commands.command(name='interval', help='Sets the speaking interval in minutes')
+    async def set_interval(self, ctx, interval):
+        try:
+            i = int(interval)
+            self.say_price_interval.cancel()
+            self.say_price_interval.change_interval(minutes=i)
+            self.say_price_interval.restart()
+            await ctx.send("Prices will be spoken every {} minutes".format(i))
+        except ValueError:
+            await ctx.send("Please enter a valid interval")
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -77,15 +217,11 @@ class ShibSayer(commands.Cog):
             # Leave
             await self.bot.voice_clients[0].disconnect()
 
-    @tasks.loop(seconds=30.0)
-    async def try_say_shib(self):
-        print("running say loop")
-        if len(self.bot.voice_clients) == 0:
-            return
-
-        await say_shib(self.bot, self.bot.voice_clients[0].channel)
+    @tasks.loop(minutes=5)
+    async def say_price_interval(self):
+        await self.say_all_prices()
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or(PREFIX),
-                   description='Welcomes users')
-bot.add_cog(ShibSayer(bot))
+                   description='Prices of Securities and Crypto')
+bot.add_cog(PriceSayer(bot))
 bot.run(TOKEN)
